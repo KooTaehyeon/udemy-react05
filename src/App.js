39f164ac62889593d086968878a1aa8fb19cb3bd
Kth <vcro12123@naver.com>
2022-05-20 18:09:57 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
 import './App.css';
-
+import DemoOutput from './components/Demo/Demo';
 function App() {
   const [showParagragh, setShowParagragh] = useState(false);
   const toggleBtn = () => {
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagragh && <p>This is new!</p>}
+      <DemoOutput show={showParagragh} />
       <Button onClick={toggleBtn}>Show Paragragh</Button>
     </div>
   );
